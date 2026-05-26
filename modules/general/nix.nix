f74d7e21364bc99578f660319617e5edd4826c1c
Szymon Groszkowski <szymongr14@gmail.com>
2026-05-26 17:54:54 +0200

    feat: add extra binary caches and trusted public keys for Nix

--- modules/general/nix.nix
+++ modules/general/nix.nix
@@ -4,6 +4,15 @@
       settings = {
         experimental-features = "nix-command flakes";
         trusted-users = [ "root" "szymongrrr" ];
+
+        # Extra binary caches — Nix downloads prebuilt artifacts from here
+        # before falling back to local build.
+        extra-substituters = [
+          "https://cache.dartkit.pl/dartkitos"
+        ];
+        extra-trusted-public-keys = [
+          "dartkitos:qbEVIC7PCAV2tfg+nUbUT9LqK30r6sdh9vOOcoiag40="
+        ];
       };
 
       gc = {
