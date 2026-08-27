eade5336483fc9e2d0d6d98d905a3e2dc8e0cf58
Szymon Groszkowski <szymongr14@gmail.com>
2026-08-27 12:23:55 +0200

    feat: add additional build machine configuration for aarch64-darwin

--- modules/hosts/darwin/remote-linux-builder.nix
+++ modules/hosts/darwin/remote-linux-builder.nix
@@ -16,12 +16,28 @@
 
       buildMachines = [
         {
+          hostName = "szymons-mac-mini.deer-bangus.ts.net";
+          protocol = "ssh-ng";
+          sshUser = "szymongrrr";
+          sshKey = "/Users/szymongrrr/.ssh/id_ed25519";
+          publicHostKey = "c3NoLWVkMjU1MTkgQUFBQUMzTnphQzFsWkRJMU5URTVBQUFBSUdKelJ2cVVqN1czcUdaM0VWWURHZ2VGU3dQM1lZK0ZVUDZZMTExUldqdXkK";
+          systems = ["aarch64-darwin"];
+          maxJobs = 2;
+          speedFactor = 2;
+          supportedFeatures = [
+            "apple-virt"
+            "benchmark"
+            "big-parallel"
+            "nixos-test"
+          ];
+        }
+        {
           hostName = "szymons-mac-mini.deer-bangus.ts.net:31022";
           protocol = "ssh-ng";
           sshUser = "builder";
           sshKey = "/Users/szymongrrr/.ssh/id_ed25519";
           publicHostKey = "c3NoLWVkMjU1MTkgQUFBQUMzTnphQzFsWkRJMU5URTVBQUFBSUpCV2N4Yi9CbGFxdDFhdU90RStGOFFVV3JVb3RpQzVxQkorVXVFV2RWQ2Igcm9vdEBuaXhvcwo=";
-          systems = [ "aarch64-linux" ];
+          systems = ["aarch64-linux"];
           maxJobs = 2;
           speedFactor = 2;
           supportedFeatures = [
